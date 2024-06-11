0876aad8c5e57b6c2a01e2464c81be6e62050c24
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-06-11 14:57:50 +0530

    Create Toggle 2.swift

---
+++ Toggle 2.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learn
+//
+//  Created by Rohan Sakhare on 08/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isOn : Bool = false
+    var body: some View {
+        VStack {
+            Toggle(isOn: $isOn, label: {
+                Text(isOn ? "ON" : "OFF")
+                    .foregroundColor(isOn ? .black : .cyan)
+            }).fixedSize()
+          
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(isOn ? .cyan : .black)
+    }
+}
+
+#Preview {
+    ContentView()
+}
